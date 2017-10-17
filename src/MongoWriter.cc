df39e222e3757c6afd90317af7dbd44f8035f8f4
logan <rs>
2017-10-16 18:09:20 -0600

    Added mongo library to prototype

--- src/MongoWriter.cc
+++ src/MongoWriter.cc
@@ -1,5 +1,19 @@
 #include "MongoWriter.h"
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
 
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
 using namespace logging;
 using namespace writer;
 
@@ -30,6 +44,7 @@ bool MongoDB::DoInit(const WriterInfo& info, int num_fields,
 
         std::cout << std::endl;
     }
+    mongocxx::instance instance{};
     return true;
 }
 
