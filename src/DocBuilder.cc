19f4cadd992706ba23b36c87a6dbc257e5a816b9
logan <rs>
2017-11-30 00:22:25 -0700

    Fixed a bug where empty fields were not set to golang zero values

--- src/DocBuilder.cc
+++ src/DocBuilder.cc
@@ -4,76 +4,45 @@
 using namespace plugin::OCMDev_MongoDBWriter;
 
 void DocBuilder::addField(const threading::Field *const field, const threading::Value *const value) {
-    if( value->present )
-    {
-        string tag = field->name;
-        std::replace( tag.begin(), tag.end(), '.', '_');
+
+    string tag = field->name;
+    std::replace( tag.begin(), tag.end(), '.', '_');
+    if (!value->present) {
         switch (value->type)
         {
+
             case TYPE_BOOL:
-                builder << tag << bsoncxx::types::b_bool{value->val.int_val};
+                builder << tag << false;
+                break;
+            case TYPE_PORT:
+                builder << tag << bsoncxx::types::b_int32{0};
                 break;
             case TYPE_INT:
-                builder << tag << bsoncxx::types::b_int64{value->val.int_val};
-                break;
             case TYPE_COUNT:
             case TYPE_COUNTER:
-                //THIS IS LARGE PROBLEM FIX
-                //TODO: Inspect RITA
-                builder << tag << bsoncxx::types::b_int64{(int64)value->val.uint_val};
+                builder << tag << bsoncxx::types::b_int64{0};
                 break;
             case TYPE_DOUBLE:
             case TYPE_TIME:
             case TYPE_INTERVAL:
-                builder << tag << bsoncxx::types::b_double{value->val.double_val};
-                break;
-            case TYPE_PORT:
-                builder << tag << bsoncxx::types::b_int32{(int)value->val.port_val.port};
+                builder << tag << bsoncxx::types::b_double{0.0};
                 break;
             case TYPE_ADDR:
-                builder << tag << formatter->Render(value->val.addr_val);
-                break;
             case TYPE_SUBNET:
-                builder << tag << formatter->Render(value->val.subnet_val);
-                break;
             case TYPE_ENUM:
             case TYPE_STRING:
             case TYPE_FILE:
             case TYPE_FUNC: {
-                                const auto length = static_cast<unsigned long>(
-                                        value->val.string_val.length
-                                        );
-                                builder << tag << string(value->val.string_val.data, length);
-                            }
-                            break;
+                const char* empty = "";
+                builder << tag << empty;
+            }
+                break;
             case TYPE_TABLE:
             case TYPE_VECTOR:
-                            {
-                                // get the details of the collection
-                                bro_int_t size = 0;
-                                threading::Value **vals = nullptr;
-
-                                if (value->type == TYPE_TABLE) {
-                                    size = value->val.set_val.size;
-                                    vals = value->val.set_val.vals;
-                                } else /*if (value->type == TYPE_VECTOR)*/ {
-                                    size = value->val.vector_val.size;
-                                    vals = value->val.vector_val.vals;
-                                }
-
-                                // build the representation recursively
-                                bool innerSuccess;
-                                string innerRep;
-
-                                using bsoncxx::builder::stream::array;
-                                auto arr = array{};
-
-                                for (bro_int_t i = 0; i < size; i++) {
-                                    addArrayField( arr,  vals[i] );
-                                }
-                                builder << tag << arr;
-                            }
-                            break;
+            {
+                builder << tag <<  bsoncxx::builder::stream::array{};
+            }
+                break;
             case TYPE_VOID:
             case TYPE_PATTERN:
             case TYPE_ANY:
@@ -84,9 +53,89 @@ void DocBuilder::addField(const threading::Field *const field, const threading::
             case TYPE_OPAQUE:
             case TYPE_TYPE:
             case TYPE_ERROR:
-                            std::cout << "Unhandled BroType" << std::endl ;
-                            break;
+                std::cout << "Unhandled BroType" << std::endl ;
+                break;
         }
+        return;
+    }
+    switch (value->type)
+    {
+        case TYPE_BOOL:
+            builder << tag << bsoncxx::types::b_bool{value->val.int_val};
+            break;
+        case TYPE_INT:
+            builder << tag << bsoncxx::types::b_int64{value->val.int_val};
+            break;
+        case TYPE_COUNT:
+        case TYPE_COUNTER:
+            //THIS IS LARGE PROBLEM FIX
+            //TODO: Inspect RITA
+            builder << tag << bsoncxx::types::b_int64{(int64)value->val.uint_val};
+            break;
+        case TYPE_DOUBLE:
+        case TYPE_TIME:
+        case TYPE_INTERVAL:
+            builder << tag << bsoncxx::types::b_double{value->val.double_val};
+            break;
+        case TYPE_PORT:
+            builder << tag << bsoncxx::types::b_int32{(int)value->val.port_val.port};
+            break;
+        case TYPE_ADDR:
+            builder << tag << formatter->Render(value->val.addr_val);
+            break;
+        case TYPE_SUBNET:
+            builder << tag << formatter->Render(value->val.subnet_val);
+            break;
+        case TYPE_ENUM:
+        case TYPE_STRING:
+        case TYPE_FILE:
+        case TYPE_FUNC: {
+                            const auto length = static_cast<unsigned long>(
+                                    value->val.string_val.length
+                                    );
+                            builder << tag << string(value->val.string_val.data, length).c_str();
+                        }
+                        break;
+        case TYPE_TABLE:
+        case TYPE_VECTOR:
+                        {
+                            // get the details of the collection
+                            bro_int_t size = 0;
+                            threading::Value **vals = nullptr;
+
+                            if (value->type == TYPE_TABLE) {
+                                size = value->val.set_val.size;
+                                vals = value->val.set_val.vals;
+                            } else /*if (value->type == TYPE_VECTOR)*/ {
+                                size = value->val.vector_val.size;
+                                vals = value->val.vector_val.vals;
+                            }
+
+                            // build the representation recursively
+                            bool innerSuccess;
+                            string innerRep;
+
+                            using bsoncxx::builder::stream::array;
+                            auto arr = array{};
+
+                            for (bro_int_t i = 0; i < size; i++) {
+                                addArrayField( arr,  vals[i] );
+                            }
+                            builder << tag << arr;
+                        }
+                        break;
+        case TYPE_VOID:
+        case TYPE_PATTERN:
+        case TYPE_ANY:
+        case TYPE_UNION:
+        case TYPE_RECORD:
+        case TYPE_LIST:
+        case TYPE_TIMER:
+        case TYPE_OPAQUE:
+        case TYPE_TYPE:
+        case TYPE_ERROR:
+                        std::cout << "Unhandled BroType" << std::endl ;
+                        break;
     }
 }
 
